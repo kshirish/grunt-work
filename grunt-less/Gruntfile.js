7b29f1e3c801441a8476681eceaca5d7943789af
kshirish <kisleyshirish25@gmail.com>
2015-08-28 16:29:44 +0530

    grunt less

---
+++ grunt-less/Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		less: {
+			dist: {
+				options: {
+					cleancss: true,
+					compress: false
+				},
+				expand: false,
+				src: 'style.less',
+				dest: 'style.css'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+
+	// Run task as -
+	// grunt less
+	// grunt less:dist
+};
