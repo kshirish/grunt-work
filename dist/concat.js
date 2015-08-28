982ee7b7cb831305ce699dd58e8b485e44097a22
kshirish <kisleyshirish25@gmail.com>
2015-08-28 12:54:00 +0530

    grunt concat

---
+++ dist/concat.js
@@ -0,0 +1,38 @@
+/*! concat - v1.0.0 - 2015-08-28 */
+var a = 45;
+
+if( a === 45 ) {
+	a = 30;
+}
+
+var tea = function() {
+	return 'your tea, sir!';
+};
+;var b = 45;
+
+if( b === 45 ) {
+	b = 30;
+}
+
+var coffee = function() {
+	return 'your coffee, sir!';
+};
+;var d = 45;
+
+if( d === 45 ) {
+	d = 30;
+}
+
+var say = function() {
+	return 'hate';
+};
+;var c = 45;
+
+if( c === 45 ) {
+	c = 30;
+}
+
+var play = function() {
+	return 'cricket';
+};
+/*! concat - v1.0.0 - 2015-08-28 */
---
+++ src/booking/two.js
@@ -0,0 +1,9 @@
+var b = 45;
+
+if( b === 45 ) {
+	b = 30;
+}
+
+var coffee = function() {
+	return 'your coffee, sir!';
+};
